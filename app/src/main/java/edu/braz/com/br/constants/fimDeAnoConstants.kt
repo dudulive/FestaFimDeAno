1d6eb2edf58b0f2fd890b994effe521bd3e5137e
dudulive <edurodriguesbraz@hotmail.com>
2018-05-14 21:58:32 -0300

    Salvando dados localmente.

---
+++ app/src/main/java/edu/braz/com/br/constants/fimDeAnoConstants.kt
@@ -0,0 +1,10 @@
+package edu.braz.com.br.constants
+
+class FimDeAnoConstants private constructor(){
+
+    companion object {
+        val PRESENCE = "presence_key"
+        val CONFIRMED_WILL_GO = "yes"
+        val CONFIRMED_WONT_GO = "no"
+    }
+}
--- app/src/main/java/edu/braz/com/br/festafimdeano/DetailsActivity.kt
+++ app/src/main/java/edu/braz/com/br/festafimdeano/DetailsActivity.kt
@@ -2,11 +2,59 @@ package edu.braz.com.br.festafimdeano
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import edu.braz.com.br.constants.FimDeAnoConstants
+import edu.braz.com.br.util.SecurityPrefences
 
-class DetailsActivity : AppCompatActivity() {
+
+
+class DetailsActivity : AppCompatActivity(), View.OnClickListener{
+
+    private val mViewHolder: ViewHolder = ViewHolder()
+    private lateinit var mSecurityPrefences: SecurityPrefences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar!!.setIcon(R.mipmap.ic_launcher)
+
+        this.mSecurityPrefences = SecurityPrefences(this)
+
+        this.mViewHolder.checkParticipate = findViewById(R.id.check_participate) as CheckBox
+
+        this.mViewHolder.checkParticipate!!.setOnClickListener(this)
+
+        this.loadDataFromActivity()
+    }
+
+    override fun onClick(view: View) {
+        val id = view.getId()
+        if (id == R.id.check_participate) {
+            if (this.mViewHolder.checkParticipate.isChecked) {
+                this.mSecurityPrefences.storeString(FimDeAnoConstants.PRESENCE, FimDeAnoConstants.CONFIRMED_WILL_GO)
+            } else {
+                this.mSecurityPrefences.storeString(FimDeAnoConstants.PRESENCE, FimDeAnoConstants.CONFIRMED_WONT_GO)
+            }
+        }
+    }
+
+    private fun loadDataFromActivity() {
+        val extras = intent.extras
+        if (extras != null) {
+            val presence = extras.getString(FimDeAnoConstants.PRESENCE)
+            if (presence == FimDeAnoConstants.CONFIRMED_WILL_GO) {
+                this.mViewHolder.checkParticipate!!.isChecked = true
+            } else {
+                this.mViewHolder.checkParticipate!!.isChecked = false
+            }
+        }
+    }
+
+    private class ViewHolder {
+        lateinit var checkParticipate: CheckBox
     }
 }
